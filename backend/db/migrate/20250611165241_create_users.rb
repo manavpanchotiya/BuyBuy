d25d118301cee8639cf3383349a41529bd613d24
manavpanchotiya <manavpanchotiya@gmail.com>
2025-06-11 12:56:26 -0500

    CORS setup

---
+++ backend/db/migrate/20250611165241_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.2]
+  def change
+    create_table :users do |t|
+      t.string :user_email
+      t.string :user_password
+      t.string :first_name
+      t.string :last_name
+      t.string :user_type
+      t.string :user_location
+
+      t.timestamps
+    end
+  end
+end
