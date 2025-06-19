5ec3fdee6027a5ed96234f3af243c0122304378e
manavpanchotiya <manavpanchotiya@gmail.com>
2025-06-19 00:04:57 -0500

    added debug

--- frontend/src/cable.js
+++ frontend/src/cable.js
@@ -3,5 +3,7 @@ import { backendURL } from "./api";
 
 const wsProtocol = backendURL.startsWith("https") ? "wss" : "ws";
 const wsURL = backendURL.replace(/^http/, wsProtocol) + "/cable";
+console.log("WebSocket URL:", wsURL);
+
 
 export default createConsumer(wsURL);
