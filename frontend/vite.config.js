d5e75a76b7be94ee9f9910d8f51abb42ee3985c3
manavpanchotiya <manavpanchotiya@gmail.com>
2025-06-18 23:53:37 -0500

    added _redirects

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [{ src: '_redirects', dest: 'dist' }],
+      hook: 'writeBundle',
+    }),
+  ],
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
